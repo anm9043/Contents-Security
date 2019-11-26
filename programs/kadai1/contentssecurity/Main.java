ae3e3cac5cacccddbce05a2150eb8cc0bb9541d4
ANAMI Taichi <daylight87m_p@yahoo.co.jp>
2019-11-26 05:28:11 +0000

    3限の作業終了

--- programs/kadai1/contentssecurity/Main.java
+++ programs/kadai1/contentssecurity/Main.java
@@ -12,25 +12,32 @@ public class Main {
         double weight[][]       = new double[6][4]; //重み行列
         double pass_or_fail[][] = new double[4][4]; //合否行列
         
-        FileInputStream   fis = new FileInputStream("seiseki.txt");
+        FileInputStream   fis = new FileInputStream("/workspace/Contents-Security/programs/kadai1/contentssecurity/seiseki.txt");
         InputStreamReader isr = new InputStreamReader((fis), "UTF-8");
         BufferedReader    br  = new BufferedReader(isr);
         
+        br.readLine();
+
+        //成績行列に値を読み込み
         for(int i = 0; i < 4; i++){
             String input = br.readLine();
             String temp[] = input.split(",", 0);
-            for(int j = 0; j < 4; j++){
-                grade[i][j] = Double.parseDouble(temp[j]);
+            for(int j = 0; j < 6; j++){
+                grade[i][j] = Double.parseDouble(temp[j + 1]);
             }
+        }
+
+        fis = FileInputStream()
+    
+        
+
+        System.out.println("        国 数 英 理 社 内");
+        
+        for(int i = 0; i < 6; i++){
+            System.out.printf("生徒%d  %3.0f%3.0f%3.0f%3.0f%3.0f%3.0f\n",
+                i + 1, grade[i][0], grade[i][1], grade[i][2], grade[i][3], grade[i][4], grade[i][5]);
         }
         
-        System.out.println("国,数,英,理,社,内");
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 6; j++){
-                System.out.printf("生徒%d  %3d%3d%3d%3d%3d%3d",
-                    i + 1, grade[i][0], grade[i][1], grade[i][2], grade[i][3], grade[i][4], grade[i][5]);
-            }
-        }
         
 	}
 }
